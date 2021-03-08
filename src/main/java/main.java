a238c6e9159057e2cc7fda0ab95cf9b92471ad6c
Rafał Lisak <rafal.lisak@appliscale.io>
2021-03-08 19:18:17 +0100

    Caluculator made

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,7 +1,10 @@
-import multiplicators.IMultiplicator;
+import generators.MatrixGenerator;
+import multiplicators.*;
 
+import java.rmi.MarshalException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class main {
 
@@ -13,22 +16,38 @@ public class main {
 
 
 class Calculator {
+
     public void run() {
+        MatrixGenerator generator = new MatrixGenerator();
+        List<Integer> integerList = List.of(10,100,1000);
         List<IMultiplicator> multiplicatorList = new ArrayList<>();
+        for(int size : integerList){
+            double[][] matrix1 = generator.generate(size);
+            double[][] matrix2 = generator.generate(size);
+            multiplicatorList.add(new IJKMultiplier(matrix1,matrix2));
+            multiplicatorList.add(new IKJMultiplier(matrix1,matrix2));
+            multiplicatorList.add(new JIKMultiplier(matrix1,matrix2));
+            multiplicatorList.add(new JKIMultiplier(matrix1,matrix2));
+            multiplicatorList.add(new KIJMultiplier(matrix1,matrix2));
+            multiplicatorList.add(new KJIMultiplier(matrix1,matrix2));
+        }
+
         for (IMultiplicator multiplicator : multiplicatorList) {
             List<Long> results = new ArrayList<>();
+            int size = 0;
             for (int i = 0; i < 10; i++) {
                 long startTime = System.nanoTime();
-                multiplicator.getResult();
+                size = multiplicator.getResult().length;
                 long stopTime = System.nanoTime();
                 Long time = stopTime - startTime;
                 results.add(time);
             }
             Long averageResult = average(results);
             String description = multiplicator.getDescription();
-            System.out.println(description + ": " + averageResult);
+            System.out.println("size "+ size +" "+description + ": " + averageResult);
         }
     }
+
 
     public Long average(List<Long> results) {
         Long sum = 0L;
